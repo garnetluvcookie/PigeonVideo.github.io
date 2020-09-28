205ea5c9b4c501b6f4c174e97a8dfbdfc39c724c
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-09-28 15:51:34 +0100

    Create basic express template
    Might fix the hosting problem thingy

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-// no
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => { 
+console.log("Your app is listening on port " + listener.address().port);
+});
